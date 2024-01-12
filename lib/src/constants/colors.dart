526cdd03fdefdc97e2be2bd3ee0e6a11763a3658
Rochmanu Purnomohadi Erfitra <155081275+nrcst@users.noreply.github.com>
2024-01-12 02:35:51 -0800

    Delete lib directory

--- lib/src/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xffa0e8af);
-const kSecondaryColor = Color(0xff86baa1);
-const kAccentColor = Color(0xff3ab795);
-
-const kWhiteColor = Color(0xffffffff);
-const kDarkColor = Color(0xff000000);
--- lib/src/constants/sizes.dart
+++
@@ -1,5 +0,0 @@
-// Default sizing
-const kDefaultSize = 30.0;
-const kSplashContainerSize = 30.0;
-const kButtonHeight = 15.0;
-const kFormHeight = 30.0;
